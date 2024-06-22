51f53bb5883827e4cd92ff44d5da5b761ff2fade
Serpent03 <dumbpilot03@gmail.com>
2024-06-22 12:47:36 +0530

    feat: new architecture

--- nn.c
+++ nn.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 /*
   New idea: instead of representing layers with structures
@@ -18,5 +19,52 @@
 
 */
 
+/* 
+  Let's get forward feed working first
+
+  List    List      List
+  Input1  Neuron1_1 Neuron2_1
+  Input2  Neuron1_2
+*/
 
 
+Neuron *init_neuron(uint32 in_nodes) {
+  /* in_nodes is determined by the number of neurons in the 
+  previous layer - as they are all interconnected. */
+  Neuron *n = (Neuron*)calloc(1, sizeof(Neuron));
+
+  n->weights = (float64*)calloc(in_nodes, sizeof(float64));
+  n->num_weights = in_nodes;
+  n->bias = 0;
+  n->value = 0;
+
+  for (uint32 i = 0; i < n->num_weights; i++) {
+    n->weights[i] = ((float64)(rand() % 100)) / 100;
+    /* randomize a value between 0 and 1. Will need a better system */
+  }
+  return n;
+}
+
+Layer *init_layer(uint32 num_neurons, uint32 in_nodes) {
+  Layer *l = (Layer*)calloc(1, sizeof(Layer));
+  l->neurons = (Neuron**)calloc(num_neurons, sizeof(Neuron*));
+  for (uint32 i = 0; i < num_neurons; i++) {
+    l->neurons[i] = init_neuron(in_nodes);
+  }
+  return l;
+}
+
+Network *init_network(uint32 *num_neurons_per_layer, uint32 num_layers) {
+  Network *n = (Network*)calloc(1, sizeof(Network));
+  n->num_layers = num_layers;
+  n->num_neurons_per_layer = (uint32*)calloc(n->num_layers, sizeof(uint32));
+  memcpy(n->num_neurons_per_layer, num_neurons_per_layer, num_layers * sizeof(uint32));
+  n->layers = (Layer**)calloc(n->num_layers, sizeof(Layer*));
+
+  /* the 0th layer is treated as the input layer. */
+  n->layers[0] = init_layer(n->num_neurons_per_layer[0], 0);
+  for (uint32 layer_ptr = 1; layer_ptr < n->num_layers; layer_ptr++) {
+    n->layers[layer_ptr] = init_layer(n->num_neurons_per_layer[layer_ptr], n->num_neurons_per_layer[layer_ptr - 1]);
+  }
+  return n;
+}
--- nn.h
+++ nn.h
@@ -19,8 +19,61 @@ typedef double float64;
 
 /* ==== STRUCTURES & METHODS ==== */
 
-typedef struct {
-  float64 weight;
+typedef struct Neuron {
+  float64 *weights; /* weights attached to the node. depends on the number of connections coming in. */
+  uint32 num_weights;
   float64 bias;
-  float64 result;
+  float64 value;
+  /* add a function pointer for activation function */
 } Neuron;
+
+typedef struct Layer {
+  Neuron **neurons; /* list containing neurons */
+  /* reserved for future..? */
+} Layer;
+
+typedef struct Network {
+  Layer **layers;
+  uint32 num_layers;
+  uint32 *num_neurons_per_layer; /* each i-th value dictates number of neurons in layer i */
+} Network;
+
+/* there has to be a function that takes in the width x breadth of the entire
+network and returns the network*/
+
+/*
+  @brief Initialize a neuron.
+  @param in_nodes The number of nuerons in the previous connecting to this neuron.
+  @return A neuron object.
+*/
+Neuron *init_neuron(uint32 in_nodes);
+
+/*
+  @brief Initialize a layer.
+  @param num_neurons Number of neurons in this layer.
+  @param in_nodes The number of neurons in the previous layer connected to each neuron in this layer.
+  @return A layer object.
+*/
+Layer *init_layer(uint32 num_neurons, uint32 in_nodes);
+
+/*
+  @brief Initialize a neural network.
+  @param num_neurons_per_layer The number of neurons per layer in the network, passed as an array.
+  The num_neurons_per_layer array must be passed via reference(pointer) and should contain information in the 
+  format [ 2, 4, 4, 1 ], which translates to: 
+  - 2 input nodes, 
+  - 4 neurons in the first hidden layer, 4 neurons in the second hidden layer,
+  - 1 neuron in the output layer.
+  @param num_layers The total number of layers, including the input, hidden and output layers.
+  @return A network object.
+*/
+Network *init_network(uint32 *num_neurons_per_layer, uint32 num_layers);
+
+/*
+  During forward propagation, the workflow will be as such:
+  >> Value gets inside <<Neuron>> after being activated.
+  >> Check the <<Layer>> structure; get current_layer + 1
+  >> Pick the first <<Neuron>> in the next layer. Iterate through the <<weights>> list
+  >> For each index i of the <<weights>> list, multiply it by the ith <<Neuron>> in the current <<Layer>>
+  >> Get the summation, and apply activation functions, and then put it in the <<Neuron>> in the next layer.
+*/
